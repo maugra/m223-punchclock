ee09ac888586acd45e450fc6b39af913dbb1c487
Maurus Graf <maurus.graf@ergon.ch>
2022-11-30 15:06:37 +0000

    add tests for update and delete

--- src/test/java/ch/zli/m223/EntryResourceTest.java
+++ src/test/java/ch/zli/m223/EntryResourceTest.java
@@ -1,10 +1,15 @@
 package ch.zli.m223;
 
-import io.quarkus.test.junit.QuarkusTest;
-import org.junit.jupiter.api.Test;
-
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
+
+import java.time.LocalDateTime;
+
+import org.junit.jupiter.api.Test;
+
+import ch.zli.m223.model.Entry;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.http.ContentType;
 
 @QuarkusTest
 public class EntryResourceTest {
@@ -17,5 +22,48 @@ public class EntryResourceTest {
              .statusCode(200)
              .body(is("[]"));
     }
+    @Test
+    public void testDeleteEndpoint() {
+      var entry = new Entry();
+      entry.setCheckIn(LocalDateTime.now());
+      entry.setCheckOut(LocalDateTime.now());
+  
+      var createResponse = given()
+        .contentType(ContentType.JSON)
+        .body(entry)
+        .when().post("/entries");
+  
+      given()
+        .when().delete("/entries/" + createResponse.jsonPath().get("id"))
+        .then()
+          .statusCode(204);
+    }
+
+    @Test
+    public void testUpdateEndpoint() {
+      var entry = new Entry();
+      entry.setCheckIn(LocalDateTime.now());
+      entry.setCheckOut(LocalDateTime.now());
+  
+      var createResponse = given()
+        .contentType(ContentType.JSON)
+        .body(entry)
+        .when().post("/entries");
+  
+  
+      var updatedEntry = new Entry();
+      updatedEntry.setCheckIn(LocalDateTime.of(2022, 06, 1, 1, 1, 1));
+      updatedEntry.setCheckOut(LocalDateTime.of(2023, 05, 1, 1, 1, 2));
+  
+      given()
+        .contentType(ContentType.JSON)
+        .body(updatedEntry)
+        .when().put("/entries/" + createResponse.jsonPath().get("id"))
+        .then()
+          .statusCode(200)
+          .body("checkIn", is("2022-06-01T01:01:01"));
+    }
+
+
 
 }
