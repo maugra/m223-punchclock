415c39b1aa5b426a44fb283582784acaa6985f47
Maurus Graf <maurus.graf@ergon.ch>
2022-11-30 15:23:41 +0000

    implement Tag entity

--- src/main/java/ch/zli/m223/model/Tag.java
+++ src/main/java/ch/zli/m223/model/Tag.java
@@ -1,5 +1,13 @@
 package ch.zli.m223.model;
 
-public class Tag {
-    
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import javax.persistence.*;public class Tag {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column(nullable = false)
+    private String title;
 }
